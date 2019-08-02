d38093f3530822d63415a4c00395858be7e5ec59
Sam Gale <samueldgale@gmail.com>
2019-08-02 15:27:05 -0700

    fixed arduino noise loop

--- taskControl.ino
+++ taskControl.ino
@@ -1,16 +1,14 @@
 int noiseInputPin = 6;
 int toneInputPin = 7;
 int soundOutputPin = 9;
-int triggerOutputPin = 2;
-int soundStartTime = 0;
+unsigned long soundStartTime = 0;
 int soundDuration = 200; // ms
-int toneFreq = 8000; // Hz
+int toneFreq = 8000;
 
 void setup() {
   pinMode(noiseInputPin,INPUT);
   pinMode(toneInputPin,INPUT);
   pinMode(soundOutputPin,OUTPUT);
-  pinMode(triggerOutputPin,OUTPUT);
 }
 
 void loop() {
@@ -18,14 +16,11 @@ void loop() {
     tone(soundOutputPin,toneFreq,soundDuration);
   }
   else if (digitalRead(noiseInputPin) == HIGH) {
-//    soundStartTime = millis();
-//    while (millis()-soundStartTime < soundDuration) {
-//      digitalWrite(soundOutputPin,random(2));
-//      delayMicroseconds(50);
-//    }
-//    digitalWrite(soundOutputPin,LOW);
-    digitalWrite(triggerOutputPin,HIGH);
-    delay(soundDuration);
-    digitalWrite(triggerOutputPin,LOW);
+    noTone(soundOutputPin);
+    soundStartTime = millis();
+    while (millis()-soundStartTime < soundDuration) {
+      digitalWrite(soundOutputPin,random(2));
+    }
+    digitalWrite(soundOutputPin,LOW);
   }
 }
