9c71c173ff0d86aab035ba232c82e91bcb073267
Sam Gale <samueldgale@gmail.com>
2019-08-02 12:17:19 -0700

    Update taskControl.ino

--- taskControl.ino
+++ taskControl.ino
@@ -1,34 +1,31 @@
 int noiseInputPin = 6;
 int toneInputPin = 7;
-int soundOutputPin = 8;
-int soundDuration = 200; // ms
+int soundOutputPin = 9;
+int triggerOutputPin = 2;
 int soundStartTime = 0;
+int soundDuration = 200; // ms
 int toneFreq = 8000; // Hz
-int toneHalfPeriod = int(1e6/toneFreq/2); // microseconds
 
 void setup() {
   pinMode(noiseInputPin,INPUT);
   pinMode(toneInputPin,INPUT);
   pinMode(soundOutputPin,OUTPUT);
+  pinMode(triggerOutputPin,OUTPUT);
 }
 
 void loop() {
   if (digitalRead(toneInputPin) == HIGH) {
     tone(soundOutputPin,toneFreq,soundDuration);
-    delay(soundDuration+10);
-//    soundStartTime = millis();
-//    while (millis()-soundStartTime < soundDuration) {
-//      digitalWrite(soundOutputPin,HIGH);
-//      delayMicroseconds(toneHalfPeriod);
-//      digitalWrite(soundOutputPin,LOW);
-//      delayMicroseconds(toneHalfPeriod);
-//    }
   }
   else if (digitalRead(noiseInputPin) == HIGH) {
-    soundStartTime = millis();
-    while (millis()-soundStartTime < soundDuration) {
-      digitalWrite(soundOutputPin,random(2));
-    }
-    digitalWrite(soundOutputPin,LOW);
+//    soundStartTime = millis();
+//    while (millis()-soundStartTime < soundDuration) {
+//      digitalWrite(soundOutputPin,random(2));
+//      delayMicroseconds(50);
+//    }
+//    digitalWrite(soundOutputPin,LOW);
+    digitalWrite(triggerOutputPin,HIGH);
+    delay(soundDuration);
+    digitalWrite(triggerOutputPin,LOW);
   }
 }
