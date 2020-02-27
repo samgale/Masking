47eaf4c6e15e172f22ca85af834097ef5694c01b
cstrawder <49964062+cstrawder@users.noreply.github.com>
2020-02-27 11:59:36 -0800

    remove print statement

--- analysis/behaviorAnalysis.py
+++ analysis/behaviorAnalysis.py
@@ -151,7 +151,6 @@ def makeWheelPlot(data, returnData=False, responseFilter=[-1,0,1], ignoreRepeats
                         if rewardFrame is not None:
                             ax.plot(trialTime[rewardFrame], trialWheel[rewardFrame], 'ro')
                         turnRightTrials.append(trialWheel)
-                        print(len(trialWheel))
                     elif rewardDirection<0:
                         ax.plot(trialTime[:trialWheel.size], trialWheel, 'b', alpha=0.2)   # plotting left turning 
                         if rewardFrame is not None:
