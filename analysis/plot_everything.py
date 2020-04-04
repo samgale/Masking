43efa6fb7cfa14cd6c0173e3abdc86cb89365541
Chelsea Strawder <chelsea.strawder@alleninstitute.org>
2020-04-03 18:51:36 -0700

    adding script to plot multiple masking resp times

--- analysis/plot_everything.py
+++ analysis/plot_everything.py
@@ -11,12 +11,11 @@ import performanceBySOA
 from plottingTargetContrast import plot_contrast
 from plotting_target_lengths import plot_flash
 from SessionPerformance import plot_session
-from dataAnalysis import import_data, create_df
 from responsePlotByParam import plot_by_param
-
+import dataAnalysis as da
 
 # choose mouse file
-d = import_data()
+d = da.import_data()
 
 # plot session wheel trace - 1 plot, unless mask==True - 2 plots
 ##  if session is from 1/13 - 1/28, use framesToShowBeforeStart=30, else 60
@@ -28,16 +27,26 @@ behaviorAnalysis.makeWheelPlot(d, responseFilter=[0],
 # plot target duration responses - 3 plots
 plot_flash(d)
 
+
 # plot contrast responses - 3 plots
 plot_contrast(d)
+
 
 # plot masking session - 3 plots 
 performanceBySOA.plot_soa(d)
 
+
 # plot activity over entire session, trial-by-trial - 1 plot
 plot_session(d)
 
+
 # plot reaction time by parameter - 1 plot
 # (df, selection='all', param='soa', stat='Median', errorBars=False)
-plot_by_param(create_df(d))
+plot_by_param(da.create_df(d))
 
+
+# plot multiple masking session response times
+files = behaviorAnalysis.get_files('486634','masking_to_analyze')    #imports ALL masking files for mouse
+dn = da.combine_files(files, '212','213','214')
+dfall = da.combine_dfs(dn)
+plot_by_param(dfall, 'soa')
