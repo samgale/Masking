b0dbbfe496e614a2fad250963b42cfa217f01b4c
Chelsea Strawder <chelsea.strawder@alleninstitute.org>
2020-04-09 16:03:39 -0700

    added param2

--- analysis/responsePlotByParam.py
+++ analysis/responsePlotByParam.py
@@ -42,7 +42,7 @@ def plot_by_param(df, selection='all', param1='soa', param2='trialLength', stat=
         print('incorrect response times\n', y)
 
  ### how to make this less bulky/redundant??     
-    param_list = [x for x in np.unique(nonzeroRxns[param]) if x >=0]   
+    param_list = [x for x in np.unique(nonzeroRxns[param1]) if x >=0]   
  
     hits = [[],[]]  #R, L
     misses = [[],[]]
@@ -52,7 +52,7 @@ def plot_by_param(df, selection='all', param1='soa', param2='trialLength', stat=
         hitVal = [[],[]]
         missVal = [[],[]]
         for j, (time, p, resp, direc, mask) in enumerate(zip(
-                nonzeroRxns['trialLength'], nonzeroRxns[param], nonzeroRxns['resp'], 
+                nonzeroRxns[param2], nonzeroRxns[param1], nonzeroRxns['resp'], 
                 nonzeroRxns['rewDir'], nonzeroRxns['maskContrast'])):
             if p==val:  
                 if direc==1:       
@@ -107,18 +107,25 @@ def plot_by_param(df, selection='all', param1='soa', param2='trialLength', stat=
             plt.errorbar(param_list, avgMisses[0], yerr=missErr[0], c='r', alpha=.3)
             plt.errorbar(param_list, avgMisses[1], yerr=missErr[1], c='b', alpha=.3)
      
-    if param=='soa' and selection=='all':
+    if param1=='soa' and selection=='all':
         avgMaskOnly = func(maskOnly)    
         ax.plot(8, avgMaskOnly, marker='o', c='k')
         param_list[0] = 8
-    
-    ax.set(title='{} Response Time From StimStart, by {}'.format(stat, param), 
-           xlabel=param.upper() + ' (ms)', ylabel='Reaction Time (ms)')
+        
+    if param2=='trialLength':
+        ylabel = 'Trial Length'
+    elif param2 == 'timeToMove':
+        ylabel = 'Time to Move Wheel'
+    elif param2 == 'timeToOutcome':
+        ylabel = 'Time to outcome'
+        
+    ax.set(title='{} Response Time From StimStart, by {}'.format(stat, param1), 
+           xlabel=param1.upper() + ' (ms)', ylabel='{} (ms)'.format(ylabel))
    # plt.suptitle((df.mouse + '   ' + df.date))  # need ot figure out loss of df metadata
     
     ax.set_xticks(param_list)   
     a = ax.get_xticks().tolist()
-    if param=='soa':
+    if param1=='soa':
         a = [int(i) for i in a if i>=0]
         a[0] = 'Mask\nOnly'
         a[-1] = 'Target\nOnly'
