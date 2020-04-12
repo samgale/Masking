01cc07c48bd64b061bdcecb549d918688a9bca12
Sam Gale <samueldgale@gmail.com>
2020-04-12 04:38:26 -0700

    Update wheelPlots_sam.py

--- analysis/wheelPlots_sam.py
+++ analysis/wheelPlots_sam.py
@@ -30,13 +30,19 @@ initiationThresh = 0.5*np.pi/180*wheelSpeedGain
 wheelPos = np.cumsum(np.stack([w[s:] for w,s in zip(df['WheelTrace'],df['stimStart']-df['trialStart']-quiescentFrames)]),axis=1)
 wheelPos -= wheelPos[:,:quiescentFrames].mean(axis=1)[:,None]
 
-soaList = np.unique(df['soa'])
-soaLabels = ['no-go','mask only']+[str(soa) for soa in soaList if soa>0]
-soaLabels[-1] = 'target only'
-soaColors = np.ones((len(soaList),4))
-soaColors[0,:3] = 0
-soaColors[1,:3] = 0.5
-soaColors[-np.sum(soaList>0):] = plt.cm.jet(np.linspace(0,1,np.sum(soaList>0)))
+param = 'soa'
+paramList = np.unique(df[param])
+if param=='soa':
+    paramList = paramList[paramList>=0]
+    paramLabels = ['no-go','mask only']+[str(soa) for soa in paramList if soa>0]
+    paramLabels[-1] = 'target only'
+    paramColors = np.ones((len(paramList),4))
+    paramColors[0,:3] = 0
+    paramColors[1,:3] = 0.5
+    paramColors[-np.sum(paramList>0):] = plt.cm.jet(np.linspace(0,1,np.sum(paramList>0)))
+else:
+    paramLabels = paramList
+    paramColors = plt.cm.jet(np.linspace(0,1,len(paramList)))
 
 t = 1000/frameRate*(np.arange(wheelPos.shape[1])-quiescentFrames)
 tinterp = np.arange(t[0],t[-1])
@@ -47,41 +53,41 @@ ax = fig.add_subplot(1,1,1)
 ymin = 0
 ymax = 0
 timeLabels = ('initiation','movement','outcome')
-responseTime = {time: {side: {resp: {soa: [] for soa in soaList} for resp in (-1,1)} for side in (-1,1)} for time in timeLabels}
+responseTime = {time: {side: {resp: {prm: [] for prm in paramList} for resp in (-1,1)} for side in (-1,1)} for time in timeLabels}
 for side in (-1,1):
     for resp in (-1,1):
-        for soa,clr,lbl in zip(soaList,soaColors,soaLabels):
-            if soa<0: # no-go
-                trials = df['nogoMove']==side if resp>0 else []
-            elif soa>0: # go
-                trials = (df['rewDir']==side) & (df['resp']==resp)
-            else: # mask only
-                trials = df['maskOnlyMove']==side if resp>0 else []
+        for prm,clr,lbl in zip(paramList,paramColors,paramLabels):
+            trials = df[param]==prm
+            if param=='soa':
+                if prm<0: # no-go
+                    trials = trials & (df['nogoMove']==side) if resp>0 else []
+                elif prm>0: # go
+                    trials = trials & (df['rewDir']==side) & (df['resp']==resp)
+                else: # mask only
+                    trials = trials & (df['maskOnlyMove']==side) if resp>0 else []  
             if any(trials):
-                trials = trials & (df['soa']==soa)
-                if any(trials):
-                    wheel = wheelPos[trials]
-                    trialsToUse = np.zeros(len(wheel),dtype=bool)
-                    for i,w in enumerate(wheel):
-                        winterp = np.interp(tinterp,t,w)
-                        winterp *= side*resp
-                        outcomeInd = np.where(winterp>rewardThresh)[0]
-                        if len(outcomeInd)>0:
-                            initInd = np.where(winterp[:outcomeInd[0]]<=initiationThresh)[0]
-                            if len(initInd)>0:
-                                initTime = tinterp[initInd][-1]+1
-                                if initTime>100:
-                                    outcomeTime = tinterp[outcomeInd[0]]
-                                    responseTime['initiation'][side][resp][soa].append(initTime)
-                                    responseTime['movement'][side][resp][soa].append(outcomeTime-initTime)
-                                    responseTime['outcome'][side][resp][soa].append(outcomeTime)
-                                    trialsToUse[i] = True
-                    if resp==1 and soa>=0:
-                        m = wheel[trialsToUse].mean(axis=0)/wheelSpeedGain*180/np.pi
-                        ymin = min(ymin,m.min())
-                        ymax = max(ymax,m.max())
-                        leg = lbl if side>0 else None
-                        ax.plot(t,m,color=clr,label=leg)
+                wheel = wheelPos[trials]
+                trialsToUse = np.zeros(len(wheel),dtype=bool)
+                for i,w in enumerate(wheel):
+                    winterp = np.interp(tinterp,t,w)
+                    winterp *= side*resp
+                    outcomeInd = np.where(winterp>rewardThresh)[0]
+                    if len(outcomeInd)>0:
+                        initInd = np.where(winterp[:outcomeInd[0]]<=initiationThresh)[0]
+                        if len(initInd)>0:
+                            initTime = tinterp[initInd][-1]+1
+                            if initTime>100:
+                                outcomeTime = tinterp[outcomeInd[0]]
+                                responseTime['initiation'][side][resp][prm].append(initTime)
+                                responseTime['movement'][side][resp][prm].append(outcomeTime-initTime)
+                                responseTime['outcome'][side][resp][prm].append(outcomeTime)
+                                trialsToUse[i] = True
+                if resp==1:
+                    m = wheel[trialsToUse].mean(axis=0)/wheelSpeedGain*180/np.pi
+                    ymin = min(ymin,m.min())
+                    ymax = max(ymax,m.max())
+                    leg = lbl if side>0 else None
+                    ax.plot(t,m,color=clr,label=leg)
                     
 ax.plot([200,200],[-2000,2000],'--',color='0.5',scaley=False)
 for side in ('right','top'):
@@ -101,13 +107,13 @@ for tlbl,alpha in zip(timeLabels[:2],(0.4,1)):
     for side,slbl in zip((-1,1),'LR'):
         for resp,rlbl,ls in zip((-1,1),('incorrect','correct'),('--','-')):
             rt = responseTime[tlbl][side][resp]
-            meanRt = [np.mean(rt[soa]) for soa in rt]
-            semRt = [np.std(rt[soa])/(len(rt[soa])**0.5) for soa in rt]
+            meanRt = [np.mean(rt[prm]) for prm in rt]
+            semRt = [np.std(rt[prm])/(len(rt[prm])**0.5) for prm in rt]
             clr = 'r' if side*resp==1 else 'b'
             lbl = tlbl+' '+slbl+' '+rlbl
-            ax.plot(soaList[1:],meanRt[1:],linestyle=ls,color=clr,alpha=alpha,label=lbl)
-            for soa,m,s in zip(soaList[1:],meanRt[1:],semRt[1:]):
-                ax.plot([soa,soa],[m-s,m+s],color=clr,alpha=alpha)
+            ax.plot(paramList,meanRt,linestyle=ls,color=clr,alpha=alpha,label=lbl)
+            for prm,m,s in zip(paramList,meanRt,semRt):
+                ax.plot([prm,prm],[m-s,m+s],color=clr,alpha=alpha)
 ax.legend(loc='upper left',fontsize=8)    
 
 
