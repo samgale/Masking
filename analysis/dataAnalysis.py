a4e7f857970907f4b1143f8215db56883a564bf0
Chelsea Strawder <chelsea.strawder@alleninstitute.org>
2020-04-14 14:09:38 -0700

    changed metadata in combined dfs

--- analysis/dataAnalysis.py
+++ analysis/dataAnalysis.py
@@ -45,7 +45,10 @@ def combine_files(files, *dates, output='df'):
 
 def combine_dfs(dict1):
     ''' to be used with dict of dataframes'''
-    return pd.concat(dict1.values(), ignore_index=True)
+    df = pd.concat(dict1.values(), ignore_index=True)
+    df.mouse = {val.mouse for val in dict1.values()}
+    df.dates = [val.date for val in dict1.values()]
+    return df
 
 
 def create_df(d):   
