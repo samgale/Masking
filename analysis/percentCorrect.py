483ef52fed82fae54a6ffa94abc61a5cfc8c58ed
Chelsea Strawder <chelsea.strawder@alleninstitute.org>
2020-09-03 13:46:15 -0700

    removing "fix"

--- analysis/percentCorrect.py
+++ analysis/percentCorrect.py
@@ -33,10 +33,8 @@ def session_stats(d, ignoreNoRespAfter=None, returnAs='str_array'):    #returnAs
     
     sessionDuration = d['trialResponseFrame'][-1]   # in frames
     
-    if d['probCatch'][()]>0:
-        catchTrials = df['catchTrial'].value_counts()[1] 
-    else:
-        catchTrials=0
+    
+    catchTrials = df['catchTrial'].value_counts()[1] if d['probCatch'][()]>0 else 0
     notCatch = df['catchTrial'].value_counts()[0]
     
     assert (len(df) - catchTrials) == (notCatch), "catch trial issue"
