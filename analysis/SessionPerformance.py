585b5869438d2231fe2f57157bb191c20cf2dbf7
Chelsea Strawder <chelsea.strawder@alleninstitute.org>
2020-09-03 16:30:38 -0700

    removed unecessary code and updated plotting
    Got rid of old no go plotting, switched to importing df instead of making new df, and added catch trial plotting

--- analysis/SessionPerformance.py
+++ analysis/SessionPerformance.py
@@ -5,12 +5,10 @@ Created on Tue Aug 13 11:20:20 2019
 @author: svc_ccg
 """
 
-import pandas as pd 
 import matplotlib
 from matplotlib import pyplot as plt
-from nogoData import nogo_turn
 import numpy as np
-from dataAnalysis import ignore_after, get_dates
+from dataAnalysis import ignore_after, get_dates, create_df
 
 """
 plots the choices (in order) over the length of a session
@@ -31,45 +29,32 @@ def plot_session(data, ion=True, ignoreNoRespAfter=10):
         plt.ion()
     
     d=data
-     
-    fi = d['frameIntervals'][:]
-    framerate = int(np.round(1/np.median(fi)))
+
+    df = create_df(d)
+    framerate = df.framerate
+    mouse = df.mouse
+    date = get_dates(df.date)
+    end = len(df)
     
-    trialResponse = d['trialResponse'][()]
-    end = len(trialResponse)-1
-    trialResponseFrame = d['trialResponseFrame'][:end]
-    trialTargetFrames= d['trialTargetFrames'][:end]   # to identify nogos 
-    trialRewardDirection = d['trialRewardDir'][:end]
-    maskOnset = d['trialMaskOnset'][:end]
-    trialMaskContrast = d['trialMaskContrast'][:end]
-    
-    for i, trial in enumerate(trialTargetFrames):
-        if trial==0:
-            trialRewardDirection[i] = 0 
-    
-    data = zip(trialRewardDirection, trialResponse, maskOnset, trialTargetFrames, trialMaskContrast)
-    
-    df = pd.DataFrame(data, index=trialResponseFrame, columns=['rewardDir', 'trialResp', 'mask', 'target', 'maskCon'])
-    df['CumPercentCorrect'] = df['trialResp'].cumsum()
+    for i in range(len(df)) :
+        if ~np.isfinite(df.loc[i, 'resp']):
+            print(i)
+            df.loc[i, 'resp']=0
+    df['CumPercentCorrect'] = df['resp'].cumsum()
     
     endAnalysis = ignore_after(d, ignoreNoRespAfter)
     
-    # add in code that gives a value for the nan rows 
+    
+    rightCorr = df[(df['resp']==1) & (df['rewDir']==1)]
+    rightMiss = df[(df['resp']==-1) & (df['rewDir']==1)]
+    rightNoResp = df[(df['resp']==0) & (df['rewDir']==1)]
 
     
-    #function? 
-    rightCorr = df[(df['trialResp']==1) & (df['rewardDir']==1)]
-    leftCorr = df[(df['trialResp']==1) & (df['rewardDir']==-1)]
-    nogoCorr = df[(df['trialResp']==1) & (df['rewardDir']==0)]
+    leftCorr = df[(df['resp']==1) & (df['rewDir']==-1)]
+    leftMiss = df[(df['resp']==-1) & (df['rewDir']==-1)]
+    leftNoResp = df[(df['resp']==0) & (df['rewDir']==-1)]
     
-    rightMiss = df[(df['trialResp']==-1) & (df['rewardDir']==1)]
-    leftMiss = df[(df['trialResp']==-1) & (df['rewardDir']==-1)]
-    nogoMiss = df[(df['trialResp']==-1) & (df['rewardDir']==0)]
-    
-    rightNoResp = df[(df['trialResp']==0) & (df['rewardDir']==1)]
-    leftNoResp = df[(df['trialResp']==0) & (df['rewardDir']==-1)]
-    
-    # add in nan trials 
+    catchTrials = df[(df['trialType']=='catch') | (df['trialType']=='catchNoOpto')]
 
     
     fig, ax = plt.subplots(figsize=[9.75, 6.5])
@@ -81,24 +66,10 @@ def plot_session(data, ion=True, ignoreNoRespAfter=10):
     ax.plot(leftMiss['CumPercentCorrect'], 'bv', ms=10, label="left miss")
     ax.plot(rightNoResp['CumPercentCorrect'], 'o', mec='r', mfc='none',  ms=10, label="right no response")
     ax.plot(leftNoResp['CumPercentCorrect'], 'o', mec='b', mfc='none', ms=10, label="left no response")
+    ax.plot(catchTrials['CumPercentCorrect'], '|', color='k', ms=10)
     
     
-    if 0 in trialTargetFrames:
-    
-        no_gos, _ = nogo_turn(d, ignoreRepeats=False, returnArray=True)[0] # False bc we want to see all the trials in order 
-        
-        nogoMiss = pd.DataFrame(nogoMiss['CumPercentCorrect'])
-        ax.plot(nogoCorr['CumPercentCorrect'], 'g^', ms=10)
-        
-        # set marker face fill style to reflect direction turned 
-        for nogo, x, direction in zip(no_gos, nogoMiss.index, nogoMiss['CumPercentCorrect']):
-            if nogo > 0:
-                plt.plot(x, direction, 'gv', ms=10, markerfacecoloralt='red', 
-                         fillstyle='left', label="no go turn right")  
-            elif nogo < 0:
-                plt.plot(x, direction, 'gv', ms=10, markerfacecoloralt='c', 
-                         fillstyle='left', label="no go turn left")
-    
+
 #    for mask,i,corr in zip(df['mask'], df.index, df['CumPercentCorrect']):
 #        if mask>0:
 #            print(mask, i, corr)
@@ -106,11 +77,10 @@ def plot_session(data, ion=True, ignoreNoRespAfter=10):
 #            ax.annotate(str(mask), xy=(i,corr), xytext=(0, 20), textcoords='offset points', fontsize=8)
             
     if endAnalysis[0] != end:
-    
         plt.vlines(endAnalysis[1], ax.get_ylim()[0], ax.get_ylim()[1], 'k', ls='--', 
                    label='End Analysis' if 'End Analysis' not in plt.gca().get_legend_handles_labels()[1] else '')
             
-    plt.suptitle(str(d).split('_')[-3:-1])
+    plt.suptitle(mouse + date)
     plt.title('Choices over the Session')
     plt.ylabel('Cumulative Rewards')
     plt.xlabel('Time in session (min)')
