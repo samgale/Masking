3fed492f990c7ab34d65e474eff56935bd5d9714
Sam Gale <samueldgale@gmail.com>
2021-09-01 18:25:24 -0700

    Update maskTaskAnalysis.py

--- maskTaskAnalysis.py
+++ maskTaskAnalysis.py
@@ -333,6 +333,35 @@ for data,ylim,ylabel in zip((respRate,fracCorr),((0,1),(0.4,1)),('Response Rate'
     ax.set_xlabel('Mask Onset Relative to Target Onset (ms)',fontsize=16)
     ax.set_ylabel(ylabel,fontsize=16)
     plt.tight_layout()
+    
+# pooled trials fraction correct
+fc = np.nansum(fracCorr*respRate,axis=1)/np.nansum(respRate,axis=1)
+nresp = np.sum(ntrials*respRate,axis=1)
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.plot(xlim,[0.5,0.5],'k--')
+ntotal = nresp.sum(axis=0)
+maskingPooledFracCorr = np.nansum(fc*nresp/ntotal,axis=0)
+ax.plot(xticks,maskingPooledFracCorr,'o',color='k',ms=12)
+maskingPooledFracCorrCi = []
+for x,n,p in zip(xticks,ntotal,maskingPooledFracCorr):
+    if p>0:
+        s = [c/n for c in scipy.stats.binom.interval(0.95,n,p)]
+        ax.plot([x,x],s,color='k')
+        maskingPooledFracCorrCi.append(s)
+    else:
+        maskingPooledFracCorrCi.append(np.nan)
+for side in ('right','top'):
+    ax.spines[side].set_visible(False)
+ax.tick_params(direction='out',top=False,right=False,labelsize=14)
+ax.set_xticks(xticks[1:-1])
+ax.set_xticklabels(xticklabels[1:-1])
+ax.set_xlim(xlim)
+ax.set_ylim([0.4,1])
+ax.set_xlabel('Mask Onset Relative to Target Onset (ms)',fontsize=16)
+ax.set_ylabel('Fraction Correct (pooled trials)',fontsize=16)
+plt.tight_layout()
 
 # wt vgat comparison
 vgatInd = slice(0,8)
@@ -808,26 +837,6 @@ for data,title in zip((respRate,fracCorr),('Response Rate','Fraction Correct')):
 fc,rt = [np.nansum(d[:,0]*respRate[:,0],axis=1)/np.nansum(respRate[:,0],axis=1) for d in (fracCorr,medianReacTime)]
 nresp = np.sum(ntrials[:,0]*respRate[:,0],axis=1)
 
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-ax.plot([8,108],[0.5,0.5],'k--')
-ntotal = nresp.sum(axis=0)
-m = np.nansum(fc*nresp/ntotal,axis=0)
-ax.plot(xticks,m,'o',color='k',ms=12,label=lbl)
-for x,n,p in zip(xticks,ntotal,m):
-    s = [c/n for c in scipy.stats.binom.interval(0.95,n,p)]
-    ax.plot([x,x],s,color='k')
-for side in ('right','top'):
-    ax.spines[side].set_visible(False)
-ax.tick_params(direction='out',top=False,right=False,labelsize=14)
-ax.set_xticks(xticks)
-ax.set_xticklabels(xticklabels)
-ax.set_xlim([8,108])
-ax.set_ylim([0.4,1])
-ax.set_xlabel('Optogenetic Light Onset Relative to Target Onset (ms)',fontsize=16)
-ax.set_ylabel('Fraction Correct (pooled trials)',fontsize=16)
-plt.tight_layout()
-
 rr = np.mean(respRate[:,0]-respRate[:,1,0,-1][:,None,None],axis=1)
 bw = 0.2
 bins = np.arange(-0.1,1.01,bw)
@@ -860,6 +869,51 @@ for data,ylim,ylabel in zip((fc,rt),((-0.02,1.02),None),('Fraction Correct','Rea
     ax.set_xlabel('Response Rate Relative to Chance',fontsize=12)
     ax.set_ylabel(ylabel,fontsize=12)
     plt.tight_layout()
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.plot([8,108],[0.5,0.5],'k--')
+ntotal = nresp.sum(axis=0)
+m = np.nansum(fc*nresp/ntotal,axis=0)
+ax.plot(xticks,m,'o',color='k',ms=12)
+for x,n,p in zip(xticks,ntotal,m):
+    s = [c/n for c in scipy.stats.binom.interval(0.95,n,p)]
+    ax.plot([x,x],s,color='k')
+for side in ('right','top'):
+    ax.spines[side].set_visible(False)
+ax.tick_params(direction='out',top=False,right=False,labelsize=14)
+ax.set_xticks(xticks)
+ax.set_xticklabels(xticklabels)
+ax.set_xlim([8,108])
+ax.set_ylim([0.4,1])
+ax.set_xlabel('Optogenetic Light Onset Relative to Target Onset (ms)',fontsize=16)
+ax.set_ylabel('Fraction Correct (pooled trials)',fontsize=16)
+plt.tight_layout()
+
+# pooled fraction correct combined with masking data   
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.plot([8,108],[0.5,0.5],'k--')
+ax.plot(xticks[2:],maskingPooledFracCorr[[1,3,4,5]],'o',color='0.5',ms=12,label='masking')
+for x,s in zip(xticks[2:],np.array(maskingPooledFracCorrCi)[[1,3,4,5]]):
+    ax.plot([x,x],s,color='0.5')
+ntotal = nresp.sum(axis=0)
+m = np.nansum(fc*nresp/ntotal,axis=0)
+ax.plot(xticks,m,'o',color='k',ms=12,label='inhibition')
+for x,n,p in zip(xticks,ntotal,m):
+    s = [c/n for c in scipy.stats.binom.interval(0.95,n,p)]
+    ax.plot([x,x],s,color='k')
+for side in ('right','top'):
+    ax.spines[side].set_visible(False)
+ax.tick_params(direction='out',top=False,right=False,labelsize=14)
+ax.set_xticks(xticks)
+ax.set_xticklabels(['-17','0','17','33','50','no\nopto'])
+ax.set_xlim([8,108])
+ax.set_ylim([0.4,1])
+ax.set_xlabel('Cortical Inhibition Relative To Visual Response Onset (ms)',fontsize=16)
+ax.set_ylabel('Fraction Correct (pooled trials)',fontsize=16)
+ax.legend(fontsize=14)
+plt.tight_layout()
 
 
 # opto masking
